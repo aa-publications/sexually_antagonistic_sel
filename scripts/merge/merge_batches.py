b2aab903024df3dfdaaeac9aeba17bd4cb078d38
Abin Abraham <abraham.abin13@gmail.com>
2019-04-22 07:33:21 -0500

    updated mergedbatches

--- scripts/merge/merge_batches.py
+++ scripts/merge/merge_batches.py
@@ -17,6 +17,8 @@ import pandas as pd
 from datetime import datetime
 
 DATE = datetime.now().strftime('%Y-%m-%d')
+DATE = "2019-04-15"
+
 
 sys.path.append('/dors/capra_lab/users/abraha1/prelim_studies/katja_biobank/scripts/qc')
 from func_rm_fids import rm_fids
@@ -39,9 +41,10 @@ DUP_FID_DIR = "/dors/capra_lab/users/abraha1/prelim_studies/katja_biobank/data/m
 dup_fid_file_prefix = os.path.join(DUP_FID_DIR, "fid_dups_{}.tsv")
 
 # OUTPUTS
-OUTPUT_DIR = "/dors/capra_lab/users/abraha1/prelim_studies/katja_biobank/data/mega_data/merge_batches"
-PLINK_FILES_TO_MERGE = os.path.join(OUTPUT_DIR, 'merge_list_plink_prefixes.txt')
-MERGED_PLINK_FILE = os.path.join(OUTPUT_DIR, 'merged_MEGA_{}'.format(DATE))
+# OUTPUT_DIR = "/dors/capra_lab/users/abraha1/prelim_studies/katja_biobank/data/mega_data/merge_batches"
+OUTPUT_DIR = "/dors/capra_lab/users/abraha1/prelim_studies/katja_biobank/data/mega_data/merge_batches/merge_batch_without_batch4"
+PLINK_FILES_TO_MERGE = os.path.join(OUTPUT_DIR, 'merge_list_without_batch4_plink_prefixes.txt')
+MERGED_PLINK_FILE = os.path.join(OUTPUT_DIR, 'merged_MEGA_no_batch4_2019-04-15')
 
 # batches
 batches = ["MEGA_ex_Array_Batch10_Cox_14_GenderDysphoria9_preQC_GRID",
@@ -78,67 +81,68 @@ def safe_mkdir(path):
 #   REMOVE DUPLICATE INDVIDUALS
 # 
 
-for batch in batches:
+# for batch in batches:
 
-    input_prefix = plink_file_prefix.format(batch)
-    dups_ids_file = dup_fid_file_prefix.format(batch)
-    pprefix = "temp_fid_dedups"
-    (no_dups_plink_prefix, plink_stdout), fam_ct, bim_ct = rm_fids(
-        dups_ids_file, input_prefix, OUTPUT_DIR, batch, prefix=pprefix)
+#     input_prefix = plink_file_prefix.format(batch)
+#     dups_ids_file = dup_fid_file_prefix.format(batch)
+#     pprefix = "temp_fid_dedups"
+#     (no_dups_plink_prefix, plink_stdout), fam_ct, bim_ct = rm_fids(
+#         dups_ids_file, input_prefix, OUTPUT_DIR, batch, prefix=pprefix)
 
 
 #
 #   MERGE IN PLINK
 #
 
-# write a txt file with plink prefix fiels to merge
-with open(PLINK_FILES_TO_MERGE, 'w') as fout:
-    for batch in batches:
-        fout.write(os.path.join(OUTPUT_DIR, pprefix+"_"+batch+"\n"))
+# # write a txt file with plink prefix fiels to merge
+# with open(PLINK_FILES_TO_MERGE, 'w') as fout:
+#     for batch in batches:
+#         fout.write(os.path.join(OUTPUT_DIR, pprefix+"_"+batch+"\n"))
 
-merge_cmd = "plink --merge-list {} --out {}".format(PLINK_FILES_TO_MERGE, MERGED_PLINK_FILE)
-shell_stdout = run_shell_cmd(merge_cmd)
+# merge_cmd = "plink --merge-list {} --out {}".format(PLINK_FILES_TO_MERGE, MERGED_PLINK_FILE)
+# shell_stdout = run_shell_cmd(merge_cmd)
 
 _, merged_plink_prefix = os.path.split(MERGED_PLINK_FILE)
+
 #
 #   DOWNSTREAM ANALYSIS
 #
 
-# -test-missing
-(vars_miss_removed_plink_prefix, rm_miss_var_stdout), fam_ct, bim_ct = test_missing(
-    MERGED_PLINK_FILE, OUTPUT_DIR, merged_plink_prefix, prefix='temp_merged')
+# # -test-missing
+# (vars_miss_removed_plink_prefix, rm_miss_var_stdout), fam_ct, bim_ct = test_missing(
+#     MERGED_PLINK_FILE, OUTPUT_DIR, merged_plink_prefix, prefix='temp_merged')
 
-# -maf filter
-(maf_filtered_plink_file, plink_stdout), fam_ct, bim_ct = maf_filter(
-    vars_miss_removed_plink_prefix, OUTPUT_DIR, merged_plink_prefix, prefix='temp_maf_filtered_')
+# # -maf filter
+# (maf_filtered_plink_file, plink_stdout), fam_ct, bim_ct = maf_filter(
+#     vars_miss_removed_plink_prefix, OUTPUT_DIR, merged_plink_prefix, prefix='final_maf_filtered_')
 
 
-# HWE SNPs all
-hwe_outputs, plink_stdout = hwe(maf_filtered_plink_file, OUTPUT_DIR, merged_plink_prefix, prefix='inter_hwe_')
-# HWE SNPs males
-hwe_outputs, plink_stdout = hwe(maf_filtered_plink_file, OUTPUT_DIR,
-                                     merged_plink_prefix, prefix='inter_hwe_males', plink_opts="--filter-males")
-# HWE SNPs females
-hwe_outputs, plink_stdout = hwe(maf_filtered_plink_file, OUTPUT_DIR,
-                                     merged_plink_prefix, prefix='inter_hwe_females', plink_opts="--filter-females")
+# # HWE SNPs all
+# hwe_outputs, plink_stdout = hwe(maf_filtered_plink_file, OUTPUT_DIR, merged_plink_prefix, prefix='inter_hwe_')
+# # HWE SNPs males
+# hwe_outputs, plink_stdout = hwe(maf_filtered_plink_file, OUTPUT_DIR,
+#                                      merged_plink_prefix, prefix='inter_hwe_males', plink_opts="--filter-males")
+# # HWE SNPs females
+# hwe_outputs, plink_stdout = hwe(maf_filtered_plink_file, OUTPUT_DIR,
+#                                      merged_plink_prefix, prefix='inter_hwe_females', plink_opts="--filter-females")
 
 # calc PCA across all individuals
-maf_filtered_plink_file='/dors/capra_lab/users/abraha1/prelim_studies/katja_biobank/data/mega_data/merge_batches/temp_maf_filtered__merged_MEGA_2019-04-12'
+maf_filtered_plink_file='/dors/capra_lab/users/abraha1/prelim_studies/katja_biobank/data/mega_data/merge_batches/merge_batch_without_batch4/final_maf_filtered__merged_MEGA_no_batch4_2019-04-15'
 pca_outputs, plink_stdout = pca(maf_filtered_plink_file, OUTPUT_DIR, merged_plink_prefix, prefix='inter_pca_')
 print(plink_stdout.splitlines())
 sys.stdout.flush()
 
 #
 #   CLEAN UP FILES
-#
+# #
 
-log_files_path = os.path.join(OUTPUT_DIR, 'log')
-temp_files_path = os.path.join(OUTPUT_DIR, 'temp')
-inter_files_path = os.path.join(OUTPUT_DIR, 'intermediate')
+# log_files_path = os.path.join(OUTPUT_DIR, 'log')
+# temp_files_path = os.path.join(OUTPUT_DIR, 'temp')
+# inter_files_path = os.path.join(OUTPUT_DIR, 'intermediate')
 
-safe_mkdir(log_files_path)
-safe_mkdir(temp_files_path)
-safe_mkdir(inter_files_path)
+# safe_mkdir(log_files_path)
+# safe_mkdir(temp_files_path)
+# safe_mkdir(inter_files_path)
 
 # for file in glob.glob(OUTPUT_DIR+"/*.log"):
 #     os.rename(file, os.path.join(log_files_path, os.path.basename(file)))
