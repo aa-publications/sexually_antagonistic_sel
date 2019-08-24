053c5293fd6917eae8fe7bc52f4c8a47bec74263
Abin Abraham <abraham.abin13@gmail.com>
2019-08-24 09:47:50 -0500

    modified scripts to accept arguments and created a wrapper to run the whole pipeline

--- scripts/check_alignment/0_seqlist_for_2bit.py
+++ scripts/check_alignment/0_seqlist_for_2bit.py
@@ -1,7 +1,12 @@
 #!/bin/python
 # This script will take plink assoc output and create a list of sequence to pull using twoBItToFa
 #
-#
+# assoc_file: full path to plink output from assoc analysis
+# this file must include the following headers:
+#       >  BP
+#       >  CHR
+#       >  P
+#       >  TEST
 #
 # Abin Abraham
 # created on: 2019-07-24 10:36:57
@@ -14,10 +19,7 @@ from datetime import datetime
 
 DATE = datetime.now().strftime('%Y-%m-%d')
 
-
-OUTPUT_DIR = "/dors/capra_lab/users/abraha1/prelim_studies/katja_biobank/results/2019_07_21_logistic/katja_results"
-OUTPUT_FILE = os.path.join(OUTPUT_DIR, 'seqList_for_twoBitToFa_{}.txt'.format(DATE))
-OUTPUT_FILE_KEY = os.path.join(OUTPUT_DIR, 'key_for_seqList_for_twoBitToFa_{}.tsv'.format(DATE))
+import argparse
 
 
 pval_thresh = 0.5*10**-5
@@ -27,24 +29,38 @@ SEPERATOR=","
 #   INPUT FILES
 ###
 
-# plink output from assoc
-# must include the following headers:
-#       >  BP
-#       >  CHR
-#       >  P
-#       >  TEST
 
-assoc_file ="/dors/capra_lab/users/abraha1/prelim_studies/katja_biobank/results/2019_07_21_logistic/katja_results/20190722_gwas_pca12_age.csv"
+### REQUIRED ARGUMENTS IN ORDER
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('assoc_file', action='store', type=str)
+parser.add_argument('flank_bp', action='store', type=int)
+parser.add_argument('output_file', action='store', type=str)
+parser.add_argument('output_file_key', action='store', type=str)
+results = parser.parse_args()
 
 
+ASSOC_FILE = results.assoc_file
+flank_bp = results.flank_bp
+OUTPUT_FILE = results.output_file
+OUTPUT_FILE_KEY = results.output_file_key
+
+
+
+
+# assoc_file ="/dors/capra_lab/users/abraha1/prelim_studies/katja_biobank/results/2019_07_21_logistic/katja_results/20190722_gwas_pca12_age.csv"
+# OUTPUT_DIR = "/dors/capra_lab/users/abraha1/prelim_studies/katja_biobank/results/2019_07_21_logistic/katja_results"
+
+# OUTPUT_FILE = os.path.join(OUTPUT_DIR, 'seqList_for_twoBitToFa_{}.txt'.format(DATE))
+# OUTPUT_FILE_KEY = os.path.join(OUTPUT_DIR, 'key_for_seqList_for_twoBitToFa_{}.tsv'.format(DATE))
 
 
 ###
 #   MAIN
 ###
+print("Running:\n{}".format(sys.argv))
 
 # select SNPS to create seqList
-as_df = pd.read_csv(assoc_file, sep=SEPERATOR)
+as_df = pd.read_csv(ASSOC_FILE, sep=SEPERATOR)
 sig_snps_df = as_df.loc[ (as_df['P'] < pval_thresh) & (as_df['P'] !=0) & (as_df['TEST'] == "ADD" ) ].copy()
 
 
@@ -53,12 +69,15 @@ sig_snps_df = as_df.loc[ (as_df['P'] < pval_thresh) & (as_df['P'] !=0) & (as_df[
 # chr1:0-189
 # [start,end)
 
-sig_snps_df['seq_start'] = sig_snps_df.BP - 26 # take 25 bases to the left of index position
-sig_snps_df['seq_end'] = sig_snps_df.BP + 27 # take 25 bases to the left of index position inclusive
+left_flank=int(flank_bp/2)
+right_flank=int(flank_bp/2)
+print("num bases on left and rigth: {}, {}".format(left_flank, right_flank))
+sig_snps_df['seq_start'] = sig_snps_df.BP - left_flank # take 25 bases to the left of index position
+sig_snps_df['seq_end'] = sig_snps_df.BP + right_flank # take 25 bases to the left of index position inclusive
 sig_snps_df['seqList'] = 'chr' + sig_snps_df.CHR.map(str) + ":" + sig_snps_df.seq_start.map(str) + "-" +sig_snps_df.seq_end.map(str)
 
 # write seqlist
 sig_snps_df['seqList'].to_csv(OUTPUT_FILE, index=False,header=False)
 sig_snps_df.to_csv(OUTPUT_FILE_KEY, index=False,header=True, sep="\t")
 
-print("wrote seqlist to {}".format(OUTPUT_DIR))
+print("wrote seqlist to {}".format(os.path.basename(OUTPUT_FILE)))
